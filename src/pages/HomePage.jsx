c6ae733dfa7a67dc8bc9384987fbfed7aca94f5b
Wasbis <abidardiyanto10@gmail.com>
2023-11-13 09:33:16 +0700

    Penambahan Page Home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,21 +7,23 @@ const HomePage = () => {
       <header className="w-100 min-vh-100 d-flex align-item-center">
         <Container className="container-homepage">
           <Row className="header-box d-flex justify-content-center align-item-center">
-            <Col>
-              <h1>
+            <Col lg="6">
+              <h1 className="mb-4">
                 Temukan <br />
                 <span>Bakat Kreatifmu</span> <br /> Bersama Kami !
               </h1>
-              <p>
+              <p className="mp-2">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Voluptatibus ipsam laboriosam, atque eum expedita omnis.
               </p>
               <button className="btn btn-danger btn-lg rounded-1 me-2">
                 Lihat Kelas
               </button>
-              <button>Lihat Promo</button>
+              <button className="btn btn-outline-danger btn-lg rounded-1 me-2">
+                Lihat Promo
+              </button>
             </Col>
-            <Col>
+            <Col lg="6">
               <img src={heroImage} alt="hero-img" />
             </Col>
           </Row>
